7263e7e7a31d6a38fd4cdc62fc319bf070138ead
Jinren Ping <jinren.ping@outlook.com>
2018-04-15 14:31:21 -0400

    Add flask web server

---
+++ search-app/config.py
@@ -0,0 +1,16 @@
+class Config(object):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
