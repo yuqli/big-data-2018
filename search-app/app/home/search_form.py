6cafba67e145a7e356921ac0ef801a4248faca89
Jinren Ping <jinren.ping@outlook.com>
2018-04-15 22:19:40 -0400

    Change input filed

--- search-app/app/home/search_form.py
+++ search-app/app/home/search_form.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
+
 
 class SearchForm(FlaskForm):
-    dataset = StringField('table')
+    dataset = SelectField(
+        'table', choices=['Ad Feature', 'User Profile', 'Behavior Log'])
     column = StringField('colums')
     value = StringField('value')
+    aggregate = SelectField('aggregate', choices=[
+                            'Max', 'Min', 'Count', 'Count Max', 'Count Min', 'Mean'])
     submit = SubmitField('Search')
