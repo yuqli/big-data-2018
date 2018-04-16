b7db156730791be8a65e779227d3b9856944eba4
Jinren Ping <jinren.ping@outlook.com>
2018-04-15 22:07:46 -0400

    style change

--- search-app/app/home/search_form.py
+++ search-app/app/home/search_form.py
@@ -2,9 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class SearchForm(FlaskForm):
-    dataset1 = StringField('dataset1')
-    dataset2 = StringField('dataset2')
-    column1 = StringField('dataset1')
-    column2 = StringField('dataset2')
+    dataset = StringField('table')
+    column = StringField('colums')
     value = StringField('value')
     submit = SubmitField('Search')
